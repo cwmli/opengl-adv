7b0823d1e3cedefe04721d979bf10a82ebfc17ea
Calvin Li <calvinl9331@gmail.com>
2014-12-14 20:19:56 -0500

    sorting

--- CameraTest/Camera.cpp
+++ CameraTest/Camera.cpp
@@ -1,61 +1,7 @@
 #include "Camera.h"
-//default constructor
-objCamera::objCamera(glm::vec3 camerapos, GLfloat yaw, GLfloat pitch):cameraDir(0.0f,0.0f,-1.0f),cameraUp(0.0f,1.0f,0.0f),cameraZoom(45.0f),cameraSpeed(0.05f){
+
+twodcam::twodcam(glm::vec3 camerapos, GLfloat yaw, GLfloat pitch) :cameraDir(0.0f, 0.0f, -1.0f), cameraUp(0.0f, 1.0f, 0.0f), cameraZoom(45.0f){
 	this->cameraPos = camerapos;
 	this->Yaw = yaw;
 	this->Pitch = pitch;
-}
-
-//process horizontal camera movement
-void objCamera::hshiftCamera(CameraMovements keyvalue, GLfloat deltaTime){
-	this->cameraSpeed = 0.005f * deltaTime;
-
-	if (keyvalue == FORWARD)
-		this->cameraPos += cameraDir*cameraSpeed;
-	if (keyvalue == BACKWARD)
-		this->cameraPos -= cameraDir*cameraSpeed;
-	if (keyvalue == LEFT)
-		this->cameraPos -= cameraRight*cameraSpeed;
-	if (keyvalue == RIGHT)
-		this->cameraPos += cameraRight*cameraSpeed;
-}
-
-void objCamera::dshiftCamera(double mouseX,double mouseY){
-	GLfloat xoffset = mouseX;
-	GLfloat yoffset = mouseY;
-
-	this->cameraSensitivity = 0.25f;
-	yoffset *= -cameraSensitivity;
-	xoffset *= cameraSensitivity;
-
-	this->Yaw += xoffset;
-	this->Pitch += yoffset;
-
-	if (Pitch > 89.0f)
-		this->Pitch = 89.0f;
-	if (Pitch < -89.0f)
-		this->Pitch = -89.0f;
-
-	updateCamera();
-}
-
-void objCamera::dzoomCamera(){}
-
-glm::mat4 objCamera::getViewMatrix(){
-	return (glm::lookAt(this->cameraPos, this->cameraPos + this->cameraDir, this->cameraUp));
-}
-
-
-/*Get camera direction after mouse input using trigonometry and
-recalculate camera Up and Right vectors.*/
-void objCamera::updateCamera(){
-	glm::vec3 tempDir;
-
-	tempDir.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
-	tempDir.y = sin(glm::radians(Pitch));
-	tempDir.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
-	this->cameraDir = glm::normalize(tempDir);
-
-	this->cameraRight = glm::normalize(glm::cross(cameraDir, cameraUp));
-	//this->cameraUp = glm::normalize(glm::cross(cameraDir, cameraRight));
 }
--- CameraTest/Camera.h
+++ CameraTest/Camera.h
@@ -1,39 +1,25 @@
-#ifndef CAMERA_OBJ
-#define CAMERA_OBJ
+#ifndef TWODCAMERA_OBJ
+#define TWODCAMERA_OBJ
 
 #include <glew.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <SDL.h>
 
-enum CameraMovements{
-	FORWARD,
-	BACKWARD,
-	LEFT,
-	RIGHT
-};
-
-class objCamera{
+class twodcam{
 public:
+	twodcam(glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 2.0f),
+		GLfloat yaw = -90.0f, GLfloat pitch = 0.0f);
+	void procTWODCAM();
+private:
 	//camera attribute
 	glm::vec3 cameraPos, cameraDir, cameraRight, cameraUp;
 	//euler attribute
-	GLfloat Yaw, Pitch;
-	//camera option attribute
-	GLfloat cameraSpeed, cameraSensitivity, cameraZoom;
-	//move normalize
-	GLfloat deltaTime = 0.0f, lastFrame = 0.0f;
-	
-	objCamera(glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 2.0f), 
-		GLfloat yaw = -90.0f, GLfloat pitch = 0.0f);
-	void hshiftCamera(CameraMovements, GLfloat);
-	void dzoomCamera();
-	void dshiftCamera(double, double);
+	GLfloat Yaw, Pitch, cameraZoom;
+
+	void focusoncenterCamera(GLfloat, GLfloat);
+	void zoomCamera();
 	glm::mat4 getViewMatrix();
-private:
-	void updateCamera();
+	void updateCamVectors();
 };
-
-
-
 #endif
