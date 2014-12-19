d0e0ae6aa05b900b3585ecfe172f2df2491c9daf
Calvin Li <calvinl9331@gmail.com>
2014-12-19 12:02:20 -0500

    Texture parameters + comments

--- CameraTest/Texture.cpp
+++ CameraTest/Texture.cpp
@@ -5,6 +5,7 @@ Texture::Texture(const GLchar* filePath){
 };
 
 void Texture::genTexture(const GLchar* filePath){
+	/*Generate a opengl texture using SOIL library and apply texture properties*/
 	//load the image file
 	unsigned char* sourceImage = SOIL_load_image(filePath,
 		&srctexture_Width, &srctexture_Height, 0, SOIL_LOAD_RGBA);
@@ -16,6 +17,10 @@ void Texture::genTexture(const GLchar* filePath){
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, srctexture_width, srctexture_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, sourceImage );
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	//unbind the texture
+	glBindTexture(GL_TEXTURE_2D, NULL);
+	//clear the image data
+	SOIL_free_image_data(sourceImage);
 }	
 
 
